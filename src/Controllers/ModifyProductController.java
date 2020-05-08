661b7d1c131885b5213550278615e1b3d8535970
Wess Lancaster <wess.lancaster@gmail.com>
2020-05-07 22:50:30 -0600

    Modify Product finished, minus some testing. :)

--- src/Controllers/ModifyProductController.java
+++ src/Controllers/ModifyProductController.java
@@ -6,10 +6,13 @@
 package Controllers;
 
 import Model.Inventory;
+import Model.Part;
 import Model.Product;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,10 +20,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -36,8 +41,6 @@ public class ModifyProductController implements Initializable {
     @FXML
     private TextField name_field;
     @FXML
-    private TextField inv_field;
-    @FXML
     private TextField price_field;
     @FXML
     private TextField max_field;
@@ -52,34 +55,56 @@ public class ModifyProductController implements Initializable {
     @FXML
     private Button save_button;
     @FXML
-    private TableView<?> parts_table;
-    @FXML
     private Button add_button;
     @FXML
     private Button delete_button;
     @FXML
-    private TableView<?> added_parts_table;
+    private TableView<Part> added_parts_table;
     private Inventory inv;
     @FXML
-    private TableColumn<?, ?> id;
+    private TextField stock_field;
     @FXML
-    private TableColumn<?, ?> name;
-    @FXML
-    private TableColumn<?, ?> stock;
-    @FXML
-    private TableColumn<?, ?> price;
+    private TableView<Part> part_table;
+    
+    private ObservableList<Part> new_parts;
+    private ObservableList<Part> old_parts;
+    private Product product;
+    
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        id_field.setDisable(true);
     }
 
     @FXML
-    private void search(MouseEvent event) {
+    private void button_search(MouseEvent event)
+    {
+        search();
     }
+    
+    @FXML
+    private void onEnter(ActionEvent event) {
+        search();
+    }
+    
+    @FXML
+    private void quick_search(KeyEvent event)
+    {
+        search();
+    }
+    
+    private void search()
+    {
+        String keyword = search_field.getText().trim();
+        ObservableList<Part> parts = inv.lookupPart(keyword);
+        part_table.setItems(parts);
+        if (parts.isEmpty())
+            search_field.requestFocus();
+    }
+
 
     @FXML
     private void cancel(MouseEvent event) throws IOException {
@@ -88,20 +113,155 @@ public class ModifyProductController implements Initializable {
 
     @FXML
     private void save(MouseEvent event) throws IOException {
-        // TODO Do your saving...
+        
+        /******* Validate Data *******/
+        
+        String name = name_field.getText().trim();
+        
+        //  Verify text boxes are filled out (we'll check numbers later)
+        if (name.isEmpty())
+        {
+            InvalidValueError("The \"Name\" field cannot be empty");
+            name_field.requestFocus();
+            return;
+        }
+        
+        //  Check stock is an int
+        try{Integer.parseInt(stock_field.getText());}
+        catch (NumberFormatException e)
+        {
+            //  Error window: "Stock must be an integer"
+            InvalidValueError("The value for \"Stock\" must be an integer.");
+            stock_field.requestFocus();
+            return;
+        }
+        int stock = Integer.parseInt(stock_field.getText());
+        
+        //  Check price is a double
+        try{Double.parseDouble(price_field.getText());}
+        catch (NumberFormatException e)
+        {
+            InvalidValueError("The value for \"Price/Cost\" must be a number.");
+            price_field.requestFocus();
+            return;
+        }
+        double price = Double.parseDouble(price_field.getText());
+        
+        //  Check if max is an int
+        try{Integer.parseInt(max_field.getText());}
+        catch (NumberFormatException e)
+        {
+            //  Error window: "Max must be an integer"
+            InvalidValueError("The value for \"Max\" must be an integer.");
+            max_field.requestFocus();
+            return;
+        }
+        int max = Integer.parseInt(max_field.getText());
+        
+        //  Check if min is an int
+        try{Integer.parseInt(min_field.getText());}
+        catch (NumberFormatException e)
+        {
+            InvalidValueError("The value for \"Min\" must be an integer.");
+            min_field.requestFocus();
+            return;
+        }
+        int min = Integer.parseInt(min_field.getText());
+
+        // Check for valid integer values: Min, Max, Stock
+        if (min < 0)
+        {
+            InvalidValueError("Min must be 0 or greater");
+            min_field.requestFocus();
+            return;
+        }
+        if (max < 1)
+        {
+            InvalidValueError("Max must be 1 or greater");
+            max_field.requestFocus();
+            return;
+        }
+        if (min > max)
+        {
+            InvalidValueError("The value for \"Min\" cannot be greater than \"Max\".");
+            min_field.requestFocus();
+            return;
+        }
+        if (stock > max || stock < min)
+        {
+            InvalidValueError("The value for \"Stock\" must be between Min and Max.");
+            stock_field.requestFocus();
+            return;
+        }
+        
+        /******* Save Data *******/
+        product.setName(name);
+        product.setPrice(price);
+        product.setStock(stock);
+        product.setMin(min);
+        product.setMax(max);
+        
+        //  Delete old parts from product
+        for (Part p : old_parts)
+            product.deleteAssociatedPart(p);
+        
+        //  Add new parts to product
+        for (Part p : new_parts)
+            product.addAssociatedPart(p);
+        
+//        inv.addProduct(product);
         
         //  Go back to MainScreen
         ChangeWindow("MainScreen", event);
     }
+    
+    private void InvalidValueError(String msg)
+    {
+        // Error window
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Invalid Value Entered");
+        alert.setContentText(msg);
+        alert.showAndWait();
+    }
 
+    //  Add a part from the part_table to the added_parts_table
     @FXML
     private void add(MouseEvent event) {
-        //TODO add Part to a list of added products, display in table. Refresh added_parts_table
+        Part p = part_table.getSelectionModel().getSelectedItem();
+        if (p == null)
+        {
+            invalidValueError("No item selected", "Please select an item, then try again.");
+            return;
+        }
+        if (new_parts.contains(p))
+        {
+            invalidValueError("You've already added this item", "The item you selected is already in the list below.");
+            return;
+        }
+        new_parts.add(p);
     }
 
     @FXML
     private void delete(MouseEvent event) {
-        //TODO remove part from list of added products, display in table. Refresh added_parts_table
+        Part sel = added_parts_table.getSelectionModel().getSelectedItem();
+        if (sel == null)
+        {
+            invalidValueError("No item selected", "Please select an item, then try again.");
+            return;
+        }
+        new_parts.remove(sel);
+    }
+    
+    //  Creates error popup
+    private void invalidValueError(String header, String msg)
+    {
+        // Error window
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(header);
+        alert.setContentText(msg);
+        alert.showAndWait();
     }
     
     private void ChangeWindow(String window, MouseEvent event) throws IOException
@@ -123,15 +283,30 @@ public class ModifyProductController implements Initializable {
         stage.show();
     }
 
-    @FXML
-    private void onEnter(ActionEvent event) {
-        // TODO
-    }
     
-    public void setup(Inventory inv, Product p)
+    public void setup(Inventory inv, Product product)
     {
         this.inv = inv;
-        // Display product
+        this.product = product;
+        new_parts = FXCollections.observableArrayList();
+        old_parts = FXCollections.observableArrayList();
+        for (Part p : product.getAllAssociatedParts())
+        {
+            new_parts.add(p);
+            old_parts.add(p);
+        }
+        
+        //  Set tables
+        part_table.setItems(inv.getAllParts());
+        added_parts_table.setItems(new_parts);
+        
+        //  Display product data
+        id_field.setText(Integer.toString(product.getId()));
+        name_field.setText(product.getName());
+        stock_field.setText(Integer.toString(product.getStock()));
+        price_field.setText(Double.toString(product.getPrice()));
+        max_field.setText(Integer.toString(product.getMax()));
+        min_field.setText(Integer.toString(product.getMin()));
     }
 
 }
